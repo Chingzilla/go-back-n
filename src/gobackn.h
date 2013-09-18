4995bf0c09bd4c64185923c320e014000cfd91da
Chris Ching <chris.ching@colorado.edu>
2013-09-18 09:10:18 -0600

    Started gobackn header file

---
+++ src/gobackn.h
@@ -0,0 +1,20 @@
+/*** gobackn.h
+ * Authors: Chris Ching and Jiwan Rosen
+ ***/
+
+#ifndef h_gobackn
+#define h_gobackn
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+
+#define BUFFSIZE 100
+
+#endif // h_gobackn
