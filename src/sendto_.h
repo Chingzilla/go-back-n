aaf5e517d103097c8634930eb9677ae3eee0a091
Chris Ching <chris.ching@colorado.edu>
2013-09-29 17:46:57 -0600

    Moved code in sendto_.h to sendto_.c

--- src/sendto_.h
+++ src/sendto_.h
@@ -11,29 +11,8 @@ static int p_threshold;
 static int c_threshold;
 static int count = 1;
 
-void init_net_lib(double probability, unsigned int seed)
-{
-	if ((probability < 0) || (probability > 1)) {
-		printf("%f : Error in setting packet lost probability!\n",probability);
-		exit(1);
-	}
-	else {
-		srand(seed);
-		p_threshold = (int)(probability * 1000);
-	}
+void init_net_lib(double probability, unsigned int seed);
 
-}
-
-int sendto_(int sock, void* message, int length, int flags, struct sockaddr* sa, int dest_len)
-{
-	int rnd;
-	double rnd_max = (double)RAND_MAX;
-
-	rnd = ((rand() / rnd_max) * 1000);
-	if (rnd > p_threshold) {
-		return sendto(sock, message, length, flags, sa, dest_len);
-	}
-	return length;
-}
+int sendto_(int sock, void* message, int length, int flags, struct sockaddr* sa, int dest_len);
 
 #endif
